f6c8da894844b46b0258ae56b389e0c2b548751f
rajamransri-blip <rajamransri@gmail.com>
2026-03-08 19:39:37 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,30 @@ import 'package:flutter/material.dart';
 import 'home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Gaming Tool',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF121212),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF1E1E1E),
+          elevation: 0,
+        ),
+        colorScheme: ColorScheme.dark(
+          primary: Colors.greenAccent.shade400,
+          secondary: Colors.purpleAccent,
+        ),
+      ),
       home: const HomePage(),
     );
   }
